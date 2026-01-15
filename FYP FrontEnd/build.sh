32c7d6e76925228aa52478e707192dd2cd19f847
asad123-oss <iasad0235@gmail.com>
2026-01-15 16:56:05 +0500

    as

---
+++ FYP FrontEnd/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# .NET 8 SDK install karne ke liye
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0
+
+# Path set karne ke liye
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$PATH:$HOME/.dotnet
+
+# Project publish karne ke liye
+# 'YourProjectName.csproj' ko apne file name se badal dein
+dotnet publish YourProjectName.csproj -c Release -o output
